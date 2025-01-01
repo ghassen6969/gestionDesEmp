d0e8a7afae60d9c65e7403cae329577833cba99f
ghassen6969 <ghassen.isamm@gmail.com>
2025-01-01 19:21:36 +0100

    prosit10

---
+++ src/ComparatorParId.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class ComparatorParId implements Comparator<Departement> {
+    @Override
+    public int compare(Departement o1, Departement o2) {
+        return o1.getId() - o2.getId();
+    }
+}
